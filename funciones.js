a1bd31cac306c77bc1b1e71882d40e9df3350b34
xfoliux <cristiangualan91@gmail.com>
2026-04-15 23:52:14 -0500

    Se finaliza taller 2 simulador de credito

--- funciones.js
+++ funciones.js
@@ -1,1 +1,48 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+function calcularDisponible(ingresos, egresos) {
+    let disponible = ingresos - egresos;//-500
+    if (disponible < 0) {// -500<0 true
+        disponible = 0;//0
+    }
+    return disponible; //500
+}
+
+function mostrarEnSpan(idSpan, valor) {
+    let componente = document.getElementById(idSpan);
+    componente.textContent = valor;
+}
+
+function mostrarEnTxt(idTxt) {
+    let componente = document.getElementById(idTxt);
+    return componente.value;
+}
+
+function calcularCapacidadPago(montoDisponible) {
+    let capacidadPago = montoDisponible / 2;
+    return capacidadPago;
+}
+
+function calcularInteresSimple(monto, tasa, plazoAnios) {
+    let interes = monto * (tasa / 100) * plazoAnios;
+    return interes;
+}
+
+function calcularTotalPagar(monto, interes) {
+    const SOLCA = 100;
+    let totalPagar = parseFloat(monto) + parseFloat(interes) + SOLCA;
+    return totalPagar;
+}
+
+function calcularCuotaMensual(total, plazoAnios) {
+    let plazoMeses = plazoAnios * 12;
+    let cuotaMensual = total / plazoMeses;
+    return cuotaMensual;
+}
+
+function aprobarCredito(capacidadPago, cuotaMensual) {
+    if (capacidadPago >= cuotaMensual) {
+        return "CREDITO APROBADO";
+    } else {
+        return "CREDITO RECHAZADO";
+    }
+}
--- simulador.js
+++ simulador.js
@@ -1,1 +1,19 @@
-//AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+function calcular() {
+    let ingresos = parseFloat(document.getElementById("txtIngresos").value);
+    let egresos = parseFloat(document.getElementById("txtEgresos").value);
+    let disponible = calcularDisponible(ingresos, egresos);
+    mostrarEnSpan("spnDisponible", disponible.toFixed(2));//500.00
+    let capacidadPagoMensual = calcularCapacidadPago(disponible);
+    mostrarEnSpan("spnCapacidadPago", capacidadPagoMensual.toFixed(2));
+    let monto = mostrarEnTxt("txtMonto");
+    let plazo = document.getElementById("txtPlazo").value;
+    let tasa = document.getElementById("txtTasaInteres").value;
+    let interes = calcularInteresSimple(monto, tasa, plazo);
+    mostrarEnSpan("spnInteresPagar", interes.toFixed(2));
+    let totalPagar = calcularTotalPagar(monto, interes);
+    mostrarEnSpan("spnTotalPrestamo", totalPagar.toFixed(2));
+    let cuotaMensual = calcularCuotaMensual(totalPagar, plazo);
+    mostrarEnSpan("spnCuotaMensual", cuotaMensual.toFixed(2));
+    let estadoCredito = aprobarCredito(capacidadPagoMensual, cuotaMensual);
+    mostrarEnSpan("spnEstadoCredito", estadoCredito);
+}
